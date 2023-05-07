dd414cec23899c2b90acc4af7998d1f984ba546f
pragmaxim <pragmaxim@gmail.com>
2023-05-07 20:39:15 +0200

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -14,6 +14,6 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   
   module.exports = {
     plugins: [
-      ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
-    ],
+      [purgecss, require('autoprefixer')],
+    ]
   };
